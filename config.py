49838fa35fbf08e936a002c926e6e4a90b729c44
nabeel14719 <136085297+nabeel14719@users.noreply.github.com>
2023-07-31 18:55:46 +0100

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
-MODEL = "yolov8s.pt" # path to model
+MODEL = "best.pt" # path to model
 # SOURCE_VIDEO_PATH="rtsp://192.168.1.3:8554/mystream"
-SOURCE_VIDEO_PATH="2_trim.mp4"
+SOURCE_VIDEO_PATH="1.mp4"
 TARGET_VIDEO_PATH = "clips/"
-TARGET_VIDEO_NAME = "output2"
-FRAMES = 25 # number of frames to process alert
+DURATION = 10 # seconds
+INITIAL_DURATION = 10 # seconds
+CONNECTION_STRING = "HostName=HubBsc1.azure-devices.net;DeviceId=raspberry_pigcount;SharedAccessKey=28b65jvliRBu+hVQGvrgrHdR1AyCoXCTlnnnSy9nXys="
+ALARM_PIN = 23
+PI_IP = '192.168.1.3'
